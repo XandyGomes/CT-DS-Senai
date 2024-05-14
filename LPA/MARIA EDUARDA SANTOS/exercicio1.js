aa6c64ef1e0052c6b81cc746383d921f120f0991
Maria Eduarda <161326323+Dudaa-Santos@users.noreply.github.com>
2024-05-14 10:14:12 -0300

    Delete LPA/MARIA EDUARDA SANTOS/exercicio1.js

--- LPA/MARIA EDUARDA SANTOS/exercicio1.js
+++
@@ -1,7 +0,0 @@
-function calcular(a,b){
-    console.log("Soma: ", a+b)
-    console.log("Subtração: ", a-b)
-    console.log("Multiplicação: ", a*b)
-    console.log("Divisão: ", a/b)
-}
-    console.log(calcular(5,6))
