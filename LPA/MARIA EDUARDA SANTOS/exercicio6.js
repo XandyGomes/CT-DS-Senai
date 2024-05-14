f992779024bbd737c49e51413e30c8b8617e0980
Maria Eduarda <161326323+Dudaa-Santos@users.noreply.github.com>
2024-05-14 10:13:19 -0300

    Delete LPA/MARIA EDUARDA SANTOS/exercicio6.js

--- LPA/MARIA EDUARDA SANTOS/exercicio6.js
+++
@@ -1,8 +0,0 @@
-function jurosSimples(capitalInicial, taxa, tempoAplicacao){
-    return console.log("Juros Simples: ", capitalInicial + (capitalInicial * taxa/100 * tempoAplicacao))
-}
-function jurosCompostos(capitalInicial, taxa, tempoAplicacao){
-    return console.log("Juros Compostos: ", capitalInicial * (1 + taxa/100) ** tempoAplicacao)
-}
-jurosSimples(100, 5, 3)
-jurosCompostos(100, 5 ,3)
