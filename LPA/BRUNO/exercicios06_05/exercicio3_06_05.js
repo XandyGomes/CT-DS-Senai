b6d9de9f7f468f657e8aa72765e4989c6af42b7c
brunopmt <106250383+brunopmt@users.noreply.github.com>
2024-05-20 14:08:56 -0300

    Delete LPA/BRUNO/exercicios06_05 directory

--- LPA/BRUNO/exercicios06_05/exercicio3_06_05.js
+++
@@ -1,9 +0,0 @@
-function calcularPotencia(base, expoente) {
-    return Math.pow(base, expoente);
-}
-
-
-let base = 2;
-let expoente = 3;
-let resultado = calcularPotencia(base, expoente);
-console.log(`${base} elevado a ${expoente} é igual a ${resultado}`);
