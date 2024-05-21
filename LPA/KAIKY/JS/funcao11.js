03da27c11b68fc354473b2b0c15997c924ea92ea
kaiky botelho <156143541+kaiky-botelho@users.noreply.github.com>
2024-05-21 07:02:41 -0300

    Add files via upload

---
+++ LPA/KAIKY/JS/funcao11.js
@@ -0,0 +1,24 @@
+function ValidarAnoBis (anoBis){
+if(anoBis <= 0){
+return true
+}
+else if (anoBis % 4 == 0) {
+    return true
+} else if(anoBis % 100 == 0){
+    return false
+}else if(anoBis %% 400 == 0){
+    return true
+}else{
+    false
+}
+}
+
+
+
+
+
+
+
+
+
+
