304c586846e88045e06a8f77b95ea40c6f4a46e2
João Pedro França <161327584+Joao-Franca@users.noreply.github.com>
2024-05-27 14:48:07 -0300

    Add files via upload

--- LPA/JOÃO PEDRO/Ex JS/exercicio12.js
+++ LPA/JOÃO PEDRO/Ex JS/exercicio12.js
@@ -1,1 +1,20 @@
 //) Faça um algoritmo que calcule o fatorial de um número. 
+function fatorial(n) {
+    let res = 1;
+    for (let i = n; i > 1; i--) {
+        res = res * i;
+    }
+    return res;
+}
+
+function fatorialRecursivo(n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * fatorialRecursivo(n - 1);
+    }
+}
+
+console.log(fatorial(6)); 
+
+
