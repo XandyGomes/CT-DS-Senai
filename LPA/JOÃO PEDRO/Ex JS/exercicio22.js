e40b89629557d11c381f5659385d4eb49616cb0a
João Pedro França <161327584+Joao-Franca@users.noreply.github.com>
2024-06-04 08:15:32 -0300

    Delete LPA/JOÃO PEDRO/Ex JS directory

--- LPA/JOÃO PEDRO/Ex JS/exercicio22.js
+++
@@ -1,9 +0,0 @@
-function Calcular(mes, valor) {
-    if (mes > 0 && mes < 13) {
-        return (valor * Math.pow((1 + (5 / 100)), (mes - 1))).toFixed(2);
-    } else {
-        return "Mês inválido";
-    }
-}
-
-console.log(Calcular(5, 100));
--- LPA/JOÃO PEDRO/Ex JS/exercicio24.js
+++
@@ -1,6 +0,0 @@
-let contador = 0
-
-while (contador < 11){
-    console.log("Hello World")
-    contador++
-}
