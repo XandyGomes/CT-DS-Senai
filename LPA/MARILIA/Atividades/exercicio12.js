0daf6c37fc2e2b2fd07b7d0f1e39cc42e3aca27a
Marilia84 <93770897+Marilia84@users.noreply.github.com>
2024-06-04 08:29:45 -0300

    Add files via upload

---
+++ LPA/MARILIA/Atividades/exercicio12.js
@@ -0,0 +1,18 @@
+function fatorial(n) {
+    let res = 1;
+    for (let i = n; i > 1; i--){
+       res = res * i;
+    }
+    return res;
+}
+console.log(fatorial(5));
+
+function fatorialRecursivo(n) {
+    if (n == 0) {
+        return 1
+    }else{
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorialRecursivo(3));
