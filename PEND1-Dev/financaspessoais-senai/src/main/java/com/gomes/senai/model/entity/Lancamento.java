ff063b35b8b98e65cf14b64bcaf2492365495100
XandyGomes <x3.computadores@hotmail.com>
2025-03-10 10:44:37 -0300

    Aula.10.03.2025

--- PEND1-Dev/financaspessoais-senai/src/main/java/com/gomes/senai/model/entity/Lancamento.java
+++ PEND1-Dev/financaspessoais-senai/src/main/java/com/gomes/senai/model/entity/Lancamento.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,35 +19,34 @@ import jakarta.persistence.Table;
 public class Lancamento {
 	
 	@Id
-	@Column(name="id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name="descricao")
+	@Column(name = "descricao")
 	private String descricao;
 	
-	@Column(name="mes")
+	@Column(name = "mes")
 	private Integer mes;
 
-	@Column(name="ano")
+	@Column(name = "ano")
 	private Integer ano;
 	
-	@Column(name="valor")
-	private BigDecimal valor;
-	
-	@Column(name="id_usuario")
+	@ManyToOne
+	@JoinColumn(name = "id_usuario")
 	private Usuario usuario;
 	
-	@Column(name="data_cadastro")
+	@Column(name = "valor")
+	private BigDecimal valor;
+	
+	@Column(name = "data_cadastro")
 	private LocalDate dataCadastro;
 	
-	@Column(name="tipo")
-	@Enumerated(value=EnumType.STRING)
+	@Column(name = "tipo")
+	@Enumerated(value = EnumType.STRING)
 	private TipoLancamento tipo;
-
-	@Column(name="status")
-	@Enumerated(value=EnumType.STRING)
+	
+	@Column(name = "status")
+	@Enumerated(value = EnumType.STRING)
 	private StatusLancamento status;
-	
-	
 }
--- PEND1-Dev/financaspessoais-senai/src/main/java/com/gomes/senai/model/entity/Usuario.java
+++ PEND1-Dev/financaspessoais-senai/src/main/java/com/gomes/senai/model/entity/Usuario.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name="usuario", schema="financas")
+@Data
 public class Usuario {
 	
 	@Id
@@ -25,84 +27,9 @@ public class Usuario {
 	@Column(name="senha")
 	private String senha;
 
-	public Long getId() {
-		return id;
+	public static void main(String[] args) {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Alexandre");
+		usuario.getNome();
 	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		if (senha == null) {
-			if (other.senha != null)
-				return false;
-		} else if (!senha.equals(other.senha))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
-	}
-
 }
