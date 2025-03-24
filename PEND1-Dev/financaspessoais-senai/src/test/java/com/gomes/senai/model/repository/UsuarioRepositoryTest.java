a6dcea97a2a0f5098dfecf29a59fbf965ba35555
XandyGomes <x3.computadores@hotmail.com>
2025-03-24 14:53:28 -0300

    Aula.24.03.2025

--- PEND1-Dev/financaspessoais-senai/src/test/java/com/gomes/senai/model/repository/UsuarioRepositoryTest.java
+++ PEND1-Dev/financaspessoais-senai/src/test/java/com/gomes/senai/model/repository/UsuarioRepositoryTest.java
@@ -1,48 +1,88 @@
 package com.gomes.senai.model.repository;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.gomes.senai.model.entity.Usuario;
 
+//@SpringBootTest
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@DataJpaTest
 public class UsuarioRepositoryTest {
 
 	@Autowired
 	UsuarioRepository repository;
-	
+
+	@Autowired
+	TestEntityManager entityManager;
+
 	@Test
-	public void deveVerificarAExistenciaDeUmEmail(){
-		//cenário
+	public void deveVerificarAExistenciaDeUmEmail() {
+		// cenário
 		Usuario usuario = criarUsuario();
-		repository.save(usuario);
-						
-		//ação/execução
+		entityManager.persist(usuario);
+
+		// ação/execução
 		boolean result = repository.existsByEmail("alexandre@senai.com.br");
-		
-		//verificação
+
+		// verificação
 		Assertions.assertThat(result).isTrue();
 	}
-	
+
 	@Test
 	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
-		//cenario
-		repository.deleteAll();
-		
-		//ação
+		// cenario
+		// repository.deleteAll();
+
+		// ação
 		boolean result = repository.existsByEmail("alexandre@senai.com.br");
-		
-		//verificação
+
+		// verificação
 		Assertions.assertThat(result).isFalse();
 	}
+
+	@Test
+	public void devePersistirUmUsuarioNaBaseDeDados() {
+		// cenario
+		Usuario usuario = criarUsuario();
+
+		// ação
+		Usuario usuarioSalvo = entityManager.persist(usuario);
+
+		// verificação
+		Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+	}
+
+	@Test
+	public void deveBuscarUmUsuarioPorEmail() {
+		// cenario
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
+		// ação/ execução
+		Optional<Usuario> result = repository.findByEmail("alexandre@senai.com.br");
+		// verificação
+		Assertions.assertThat(result.isPresent()).isTrue();
+	}
 	
+	@Test
+	public void deveRetornarVazioAoBuscarUsuarioPorEmailQuandoNaoExisteNaBase() {
+		//verificacao
+		Optional<Usuario> result = repository.findByEmail("alexandre@senai.com.br");	
+		Assertions.assertThat(result.isPresent()).isFalse();
+	}
+
 	public static Usuario criarUsuario() {
 		return Usuario
 				.builder()
